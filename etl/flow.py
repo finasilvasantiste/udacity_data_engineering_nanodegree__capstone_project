6ff767bd1c63295830bda0ec7aa0ed5ffe702989
Fina Silva-Santisteban <fina.santiste@gmail.com>
2022-02-12 11:59:19 -0800

    load calendar data to staging table

--- etl/flow.py
+++ etl/flow.py
@@ -6,6 +6,6 @@ from etl.staging_tasks import create_calendar_staging_table,\
 
 with Flow("airbnb-tokyo-covid-data") as flow:
     create_calendar_staging_table()
-    # load_calendar_data_into_staging_table()
+    load_calendar_data_into_staging_table()
 
 # flow.run()
--- etl/staging_tasks.py
+++ etl/staging_tasks.py
@@ -4,11 +4,30 @@ from aws.s3 import print_s3_bucket_content
 import configparser
 import psycopg2
 import redshift_connector
+import json
 
 
 config = configparser.ConfigParser()
 config.read_file((open(r'dwh.cfg')))
 
+
+# CONVENIENCE METHOD
+def load_iam_role_arn():
+    """
+    Loads iam role arn.
+    :return:
+    """
+    # Opening JSON file
+    f = open('aws_role_arn.json', )
+
+    # returns JSON object as
+    # a dictionary
+    data = json.load(f)
+
+    # Closing file
+    f.close()
+
+    return data['iam_role_arn']
 
 @task
 def create_calendar_staging_table():
@@ -31,7 +50,6 @@ CREATE TABLE IF NOT EXISTS tokyo_airbnb_calendar (
 
     print(sql_query_create_table)
 
-    # Connects to Redshift cluster using AWS credentials
     conn = redshift_connector.connect(
         host=host,
         database=db_name,
@@ -46,35 +64,37 @@ CREATE TABLE IF NOT EXISTS tokyo_airbnb_calendar (
 
 @task
 def load_calendar_data_into_staging_table():
-    iam_role_arn = config.get('REDSHIFT_CLUSTER', 'ADMIN_USER_ARN')
-    db_name = config.get('REDSHIFT_CLUSTER', 'DB_NAME')
-    user_name = config.get('REDSHIFT_CLUSTER', 'USER_NAME')
-    password = config.get('REDSHIFT_CLUSTER', 'PASSWORD')
-    host = config.get('REDSHIFT_CLUSTER', 'HOST')
+    iam_role_arn = load_iam_role_arn()
     file_path = config.get('S3', 'CALENDAR_DATA')
 
-    copy_sql_query = """
-    copy tokyo_airbnb_calendar
-    from '{}'
-    iam_role '{}'
-    Csv NOLOAD
-    IGNOREHEADER 1;
-    """.format(file_path, iam_role_arn)
+    db_name = config.get('REDSHIFT_CLUSTER', 'DB_NAME')
+    db_user_name = config.get('REDSHIFT_CLUSTER', 'USER_NAME')
+    password = config.get('REDSHIFT_CLUSTER', 'PASSWORD')
+    host = config.get('REDSHIFT_CLUSTER', 'HOST')
+    user = config.get('AWS_CREDS_ADMIN', 'USER_NAME')
+    cluster_identifier = config.get('REDSHIFT_CLUSTER', 'CLUSTER_IDENTIFIER')
+    access_key_id = config.get('AWS_CREDS_ADMIN', 'AWS_ACCESS_KEY_ID')
+    secret_access_key = config.get('AWS_CREDS_ADMIN', 'AWS_SECRET_ACCESS_KEY')
+    region = config.get('AWS_CREDS_ADMIN', 'AWS_REGION')
 
-    print(copy_sql_query)
+    sql_query_copy_data = """
+    COPY tokyo_airbnb_calendar 
+    FROM '{}' 
+    credentials 'aws_iam_role={}' 
+    CSV IGNOREHEADER 1 compupdate off region '{}';
+    """.format(file_path, iam_role_arn, region)
 
-    try:
-        # Connects to Redshift cluster using AWS credentials
-        conn = redshift_connector.connect(
-            host=host,
-            database=db_name,
-            user=user_name,
-            password=password
-        )
+    print(sql_query_copy_data)
 
-        cursor: redshift_connector.Cursor = conn.cursor()
-        cursor.execute(copy_sql_query)
-        conn.close()
+    conn = redshift_connector.connect(
+        host=host,
+        database=db_name,
+        user=db_user_name,
+        password=password
+    )
 
-    except Exception as e:
-        print(e)
+    cursor: redshift_connector.Cursor = conn.cursor()
+    cursor.execute(sql_query_copy_data)
+    conn.commit()
+
+
