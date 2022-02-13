6f1a3621c9fc246e46c807abb2f9756d034bf321
Fina Silva-Santisteban <fina.santiste@gmail.com>
2022-02-13 15:32:12 -0800

    edit chart

--- chart_example/main.py
+++ chart_example/main.py
@@ -1,19 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from etl.utilities.db_utility import execute_sql_query_and_get_result_as_df
 
 
 def create_line_chart():
-    x = [1, 2, 3, 4, 5]
-    y = [3, 3, 3, 3, 3]
+    query ='''
+    SELECT * FROM fact_tokyo_listings_availability_and_covid_rates;
+    '''
+    data_df = execute_sql_query_and_get_result_as_df(query)
+    data_df.plot().get_figure().savefig('line_chart.png')
 
-    # plot lines
-    plt.plot(x, y, label="line 1")
-    plt.plot(y, x, label="line 2")
-    plt.plot(x, np.sin(x), label="curve 1")
-    plt.plot(x, np.cos(x), label="curve 2")
-    plt.legend()
-    # plt.show()
-    plt.savefig("line_chart.png")
+    # x = [1, 2, 3, 4, 5]
+    # y = [3, 3, 3, 3, 3]
+    #
+    # # plot lines
+    # plt.plot(x, y, label="line 1")
+    # plt.plot(y, x, label="line 2")
+    # plt.plot(x, np.sin(x), label="curve 1")
+    # plt.plot(x, np.cos(x), label="curve 2")
+    # plt.legend()
+    # # plt.show()
+    # plt.savefig("line_chart.png")
 
 
 if __name__ == "__main__":
